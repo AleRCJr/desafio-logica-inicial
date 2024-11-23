97ba8c5b25dac8d741c5b221b01313581bc943bb
AleRCJr <alessandrorcj@hotmail.com>
2024-11-23 17:04:09 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("OI")
+let nome = "CR7"
+let xp = "11000"
+
+
+    if (xp < 1000) {
+      console.log (nome + "é de nível Ferro")
+    } else if (xp <= 2000) {
+        console.log (nome + "é de nível Bronze")
+    } else if (xp <= 5000) {
+        console.log (nome + "é de nível Prata")
+    } else if (xp <= 6000) {
+        console.log (nome + "é de nível Ouro")
+    } else if (xp <= 8000) {
+      rconsole.log (nome + "é de nível Platina")
+    } else if (xp <= 9000) {
+        console.log (nome + "é de nível Ascendente")
+    } else if (xp <= 10000) {
+        console.log (nome + "é de nível Imortal")
+    } else {
+        console.log (nome + "é de nível Radiante")
+    }
+
